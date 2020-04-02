a540eb5cd58a4a05adc9cd97296bf933edc38119
Anders Bengtsson <anders.bengtsson@conrab.se>
2020-04-02 13:54:50 +0200

    Updates

---
+++ precache-manifest.d1c6bbe96719a0fb66c011c5c765bbb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6ab9691131827481d38026eeeb1960",
+    "url": "/soccer-training/index.html"
+  },
+  {
+    "revision": "abcaf5ae8081e38f5e31",
+    "url": "/soccer-training/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "f40746945d6b4f3caf73",
+    "url": "/soccer-training/static/css/main.b446fc34.chunk.css"
+  },
+  {
+    "revision": "abcaf5ae8081e38f5e31",
+    "url": "/soccer-training/static/js/2.2550099a.chunk.js"
+  },
+  {
+    "revision": "f40746945d6b4f3caf73",
+    "url": "/soccer-training/static/js/main.4c3d4b6b.chunk.js"
+  },
+  {
+    "revision": "8abad10538ce37de2f63",
+    "url": "/soccer-training/static/js/runtime~main.3752efe7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/soccer-training/precache-manifest.5e459ca23f656c3de7e56412bc3a5515.js"
+  "/soccer-training/precache-manifest.d1c6bbe96719a0fb66c011c5c765bbb0.js"
 );
 
 self.addEventListener('message', (event) => {
