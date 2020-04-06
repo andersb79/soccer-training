bd2b0a39186b09d3e57a1121f68fad60043df51c
Anders Bengtsson <anders.bengtsson@conrab.se>
2020-04-06 14:39:11 +0200

    a

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,11 +1,40 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
-import SecurityIcon from "@material-ui/icons/Security";
 import { Image } from "cloudinary-react";
 var Carousel = require("react-responsive-carousel").Carousel;
 
 function ImageCarousel({ list, images }) {
   const [selectedItem, setSelectedItem] = useState(0);
+  console.log("list", list);
+  console.log("images", images);
+
+  if (list) {
+    return (
+      <div>
+        <Carousel
+          selectedItem={selectedItem}
+          // onChange={onChange}
+          showThumbs={false}
+          showArrows={true}
+          showIndicators={true}
+          showStatus={false}
+        >
+          {list &&
+            list.map(
+              (publicId, index) =>
+                publicId && (
+                  <Image
+                    cloudName="deolievif"
+                    publicId={publicId}
+                    width="100%"
+                    height="100%"
+                  />
+                )
+            )}
+        </Carousel>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -17,27 +46,17 @@ function ImageCarousel({ list, images }) {
         showIndicators={true}
         showStatus={false}
       >
-        {list &&
-          list.map(
-            (publicId, index) =>
-              publicId && (
-                <Image
-                  cloudName="deolievif"
-                  publicId={publicId}
-                  width="100%"
-                  height="100%"
-                />
-              )
-          )}
         {images &&
           images.map(
             (item, index) =>
-              item && (
+              item.src && (
                 <img
                   key={item.id}
                   className="previewImage"
                   id="image"
                   src={item.src}
+                  width="100%"
+                  height="100%"
                 />
               )
           )}
